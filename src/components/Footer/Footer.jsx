14e34d4b0f4ac8cd5fd1ee9f895b657256aa72db
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-01-11 14:06:02 +0600

    heere some change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,26 +35,7 @@ const Footer = () => {
       <div className="bg-black/40 min-h-[400px]">
         <div className="container grid md:grid-cols-3 pb-20 pt-5">
           {/* company details */}
-          <div className="py-8 px-4">
-            <a
-              href="#"
-              className="font-semibold tracking-widest text-2xl sm:text-3xl font-cursive
-"
-            >
-              Coffee Cafe
-            </a>
-            <p className="  pt-4">
-              Crafted Coffee, Cozy Vibes, Unforgettable Moments – Your Perfect
-              Espresso Escape
-            </p>
-            <a
-              href="https://www.youtube.com/channel/UC1H-a1MKEFXRiFlGNLcy7gQ"
-              target="_blank"
-              className="inline-block bg-[#3d2517] py-2 px-4 mt-5 text-sm rounded-full"
-            >
-              Visit our YouTube Channel
-            </a>
-          </div>
+         
 
           {/* Footer links */}
           <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
@@ -100,18 +81,18 @@ const Footer = () => {
                 Address
               </h1>
               <div>
-                <p className="mb-3">Noida , Uttar Pradesh</p>
-                <p>+91 1234567890</p>
+                <p className="mb-3">Nahid , Shahrasti, Chandpur, Bangladesh</p>
+                <p>+8801889274145</p>
 
                 {/* social links */}
                 <div className="flex items-center gap-3 mt-6">
-                  <a href="#">
+                  <a href="https://github.com/nahidulhasan4">
                     <FaInstagram className="text-3xl hover:text-primary duration-300" />
                   </a>
-                  <a href="#">
+                  <a href="https://github.com/nahidulhasan4">
                     <FaFacebook className="text-3xl hover:text-primary duration-200" />
                   </a>
-                  <a href="#">
+                  <a href="https://github.com/nahidulhasan4">
                     <FaLinkedin className="text-3xl hover:text-primary duration-200" />
                   </a>
                 </div>
